db01a3b54958e53a029cba82a9be969b9146bace
Huerte <huertejerald@gmail.com>
2026-07-10 17:42:30 +0800

    chore: add a unit test for model to be verified

--- backend/database/factories/TagFactory.php
+++ backend/database/factories/TagFactory.php
@@ -12,19 +12,20 @@ class TagFactory extends Factory
 
     public function definition(): array
     {
+        $protocols = ['epc', '6b', 'gb'];
         return [
             'scan_session_id' => ScanSession::factory(),
-            'protocol' => $this->faker->randomElement(['epc', '6b', 'gb']),
-            'epc' => strtoupper($this->faker->regexify('[A-F0-9]{24}')),
-            'tid' => strtoupper($this->faker->regexify('[A-F0-9]{24}')),
-            'user_data' => strtoupper($this->faker->regexify('[A-F0-9]{16}')),
-            'antenna' => $this->faker->numberBetween(1, 4),
-            'ant1' => $this->faker->numberBetween(0, 50),
-            'ant2' => $this->faker->numberBetween(0, 50),
-            'ant3' => $this->faker->numberBetween(0, 50),
-            'ant4' => $this->faker->numberBetween(0, 50),
-            'rssi' => $this->faker->randomFloat(2, -90, -30),
-            'scanned_at' => $this->faker->dateTimeThisMonth(),
+            'protocol' => $protocols[array_rand($protocols)],
+            'epc' => strtoupper(bin2hex(random_bytes(12))),
+            'tid' => strtoupper(bin2hex(random_bytes(12))),
+            'user_data' => strtoupper(bin2hex(random_bytes(8))),
+            'antenna' => rand(1, 4),
+            'ant1' => 0,
+            'ant2' => 0,
+            'ant3' => 0,
+            'ant4' => 0,
+            'rssi' => round(rand(-9000, -3000) / 100, 2),
+            'scanned_at' => now()->subDays(rand(0, 30)),
         ];
     }
 }
---
+++ backend/tests/Feature/StandaloneIngestTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Events\TagScanned;
+use App\Models\Reader;
+use App\Models\ScanSession;
+use App\Models\Tag;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Event;
+use Tests\TestCase;
+
+class StandaloneIngestTest extends TestCase
+{
+    // I SHALL RETURN BOY!!!
+}
