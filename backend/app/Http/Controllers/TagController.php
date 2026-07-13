6e71042a0eabdfed7247d522e1faa202e7dc956b
Huerte <huertejerald@gmail.com>
2026-07-13 09:43:55 +0800

    chore: remove unused import

--- backend/app/Http/Controllers/TagController.php
+++ backend/app/Http/Controllers/TagController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Tag;
-use App\Models\ScanSession;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\StreamedResponse;
